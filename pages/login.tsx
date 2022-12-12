e13f625553e8a7bf824084ea98ff3bc4d07b92b8
maki <murasame.asa.73@gmail.com>
2022-12-12 12:16:16 +0900

    fix: ログイン画面のcssの修正

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,10 +3,15 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import styles from '../styles/login.module.css'
 import {
+  Box,
  Button,
+ Center,
+ Flex,
  FormLabel,
+ Highlight,
  Input,
  Text,
+ Grid, GridItem,
 } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signInWithRedirect } from 'firebase/auth'
@@ -66,43 +71,87 @@ const login = () => {
 
   return (
     <>
-    <div>
-      <div>
-        <Text pb="20px" fontSize='2xl'>ログイン</Text>
+      <Box 
+        w="350px" 
+        m="50px auto" 
+        p="30px 40px 40px 40px" 
+        border="solid 1px rgb(220, 220, 220)" 
+        borderRadius="20px"
+      >
+        <Text 
+          pb="20px" 
+          fontSize='2xl' 
+          fontWeight="700"
+        >
+          ログイン
+        </Text>
         <form onSubmit={handleEmailLogin}>
-            {error && <p style={{color: 'red'}}>{error}</p>}
-            <div>
-              <FormLabel>Email address</FormLabel>
-              <Input 
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="email"
-                />
-            </div>
-            <div>
-              <FormLabel>password</FormLabel>
-              <Input 
-                  id="password"
-                  name="password"
-                  type="password"
-                  placeholder="password"
-                />
-              </div>
-
-            <div className="login-button">
-            <Button type='submit' _hover={{backgroundColor:"rgba(206, 255, 0, 0.5)"}}>ログイン</Button>
-            </div>
-          </form>
-            <Button onClick={handleGoogleLogin} type='submit' _hover={{backgroundColor:"rgba(206, 255, 0, 0.5)"}}>Googleログイン</Button>
-            <div className="link-to-signup-area">
-              ユーザー登録は<Link href={"/signup"}>こちら</Link>から
-            </div>
-        </div>
-
-      </div>
-    
-   
+          {error && <p style={{color: 'red'}}>{error}</p>}
+          <Box>
+            <FormLabel>Email address</FormLabel>
+            <Input 
+                id="email"
+                name="email"
+                type="email"
+                placeholder="email"
+              />
+          </Box>
+          <Box mt="10px">
+            <FormLabel>password</FormLabel>
+            <Input 
+                id="password"
+                name="password"
+                type="password"
+                placeholder="password"
+              />
+          </Box>
+          <Button 
+            type='submit' 
+            _hover={{backgroundColor:"rgba(206, 255, 0, 0.5)"}}
+            mt="20px"
+            w="100%"
+          >
+            ログイン
+          </Button>
+        </form>
+        <Center mt="10px">
+            ユーザー登録は
+              <span className={styles.signup_link}>
+                <Link href={"/signup"}>
+                  こちら
+                </Link>
+              </span>
+            から
+        </Center>
+        <Grid 
+          templateColumns='repeat(3, 1fr)' 
+          gap={1} 
+          placeItems="center" 
+          m="20px 0 10px 0"
+        >
+          <GridItem 
+            colSpan={1} 
+            w='100%' 
+            h='1px' 
+            bg='rgb(220, 220, 220)' 
+          />
+          <Center>または</Center>
+          <GridItem 
+            colStart={3} 
+            w='100%' 
+            h='1px' 
+            bg='rgb(220, 220, 220)' 
+          />
+        </Grid>
+        <Button 
+          onClick={handleGoogleLogin} 
+          type='submit' 
+          _hover={{backgroundColor:"rgba(206, 255, 0, 0.5)"}}
+          w="100%"
+        >
+          Googleでログイン
+        </Button>
+      </Box>
     </>
   )
 }
